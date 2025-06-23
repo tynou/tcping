61ce11c4e813370331d48968fbb5c65d18115605
Alexander Timonin <2sdaiwxet@gmail.com>
2025-06-24 00:08:41 +0500

    test fix

--- tests/test_ping.py
+++ tests/test_ping.py
@@ -9,7 +9,7 @@ from response import Response
 class TestPing(unittest.TestCase):
     def test_ping_timeout(self):
         with patch("socket.socket") as mock_socket:
-            ping = Ping("127.0.0.1", 54321, "127.0.0.1", 80, 1, False)
+            ping = Ping("127.0.0.1", 54321, "127.0.0.1", 80, 1, False, False)
 
             mock_socket.return_value.recvfrom.side_effect = socket.timeout()
 
@@ -24,7 +24,7 @@ class TestPing(unittest.TestCase):
 
     def test_port_open(self):
         with patch("socket.socket") as mock_socket:
-            ping = Ping("127.0.0.1", 54321, "127.0.0.1", 80, 1, False)
+            ping = Ping("127.0.0.1", 54321, "127.0.0.1", 80, 1, False, False)
 
             fake_response = self._build_fake_response(1, 0x12)
 
@@ -42,7 +42,7 @@ class TestPing(unittest.TestCase):
 
     def test_port_closed(self):
         with patch("socket.socket") as mock_socket:
-            ping = Ping("127.0.0.1", 54321, "127.0.0.1", 80, 1, False)
+            ping = Ping("127.0.0.1", 54321, "127.0.0.1", 80, 1, False, False)
 
             fake_response = self._build_fake_response(1, 0x04)
 
@@ -61,26 +61,26 @@ class TestPing(unittest.TestCase):
     def test_ping_count(self):
         with patch.object(Ping, "ping") as mock_ping, patch("time.sleep") as mock_sleep:
             mock_ping.return_value = (Response.TIMEOUT, 0)
-            ping = Ping("127.0.0.1", 54321, "127.0.0.1", 80, 1, False)
+            ping = Ping("127.0.0.1", 54321, "127.0.0.1", 80, 1, False, False)
             ping.start(5, 1)
             self.assertEqual(mock_ping.call_count, 5)
 
     def test_ping_interval(self):
         with patch.object(Ping, "ping") as mock_ping, patch("time.sleep") as mock_sleep:
             mock_ping.return_value = (Response.TIMEOUT, 0)
-            ping = Ping("127.0.0.1", 54321, "127.0.0.1", 80, 1, False)
+            ping = Ping("127.0.0.1", 54321, "127.0.0.1", 80, 1, False, False)
             ping.start(1, 1)
             mock_sleep.assert_called_with(1)
 
     def test_build(self):
-        ping = Ping("127.0.0.1", 54321, "127.0.0.1", 80, 1, False)
+        ping = Ping("127.0.0.1", 54321, "127.0.0.1", 80, 1, False, False)
         self.assertEqual(
             ping.build(1, 2),
             b"\xd41\x00P\x00\x00\x00\x01\x00\x00\x00\x00P\x02\x04\x00\xd9]\x00\x00",
         )
 
     def test_checksum(self):
-        ping = Ping("127.0.0.1", 54321, "127.0.0.1", 80, 1, False)
+        ping = Ping("127.0.0.1", 54321, "127.0.0.1", 80, 1, False, False)
         packet = ping.build(1, 2)
         self.assertEqual(get_checksum(packet), 7422)
 
